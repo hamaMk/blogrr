6eea20ba36ec0dd685e0c0c9d5e72882cbaca1d9
hamaMk <hamandishemk2014@gmail.com>
2023-03-28 15:32:03 +0200

    feat: Updated BlogPost administration pages
    When editing/creating a post hide the owner and publish date,
    instead update these fields automatically on saving the forms.

--- api/admin.py
+++ api/admin.py
@@ -8,10 +8,17 @@ from tinymce.widgets import TinyMCE
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'creation_date', 'last_edited', 'pub_date', 'published', 'owner')
     list_filter = ('published',)
+    exclude = ('owner', 'pub_date')
 
     formfield_overrides = {
         models.TextField: {'widget': TinyMCE()},
     }
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            # Only set added_by during the first save.
+            obj.added_by = request.user
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(Tag)
