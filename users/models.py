cb8960a13e9cdcd10e31ce12c857fccb450cb971
hamaMk <hamandishemk2014@gmail.com>
2023-03-28 11:40:21 +0200

    feat: Add customer user model with role support
    The API can now use role based permissions to manage Authourization.

---
+++ users/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from .managers import AppUserManager
+
+
+class AppUser(AbstractBaseUser, PermissionsMixin):
+    AUTHOR = "AUT"
+    READER = "RDR"
+
+    ROLE_CHOICES = [
+        (AUTHOR, 'Author'),
+        (READER, 'Reader'),
+    ]
+
+    email = models.EmailField("email address", unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    role = models.CharField(
+        max_length=3,
+        choices=ROLE_CHOICES,
+        default=READER,
+    )
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = AppUserManager()
+
+    def __str__(self):
+        return f"{self.role}: {self.email}"
