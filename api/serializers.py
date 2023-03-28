adcca1cf31f048b280e1da68f0a3eee240fd5ee1
hamaMk <hamandishemk2014@gmail.com>
2023-03-28 14:02:27 +0200

    feat: Add hyperlinked model serializers to the api
    Added Users, BlogPost and Tag serializers for object-json conversions.

---
+++ api/serializers.py
@@ -0,0 +1,22 @@
+from users.models import AppUser
+from models import BlogPost, Tag
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = AppUser
+        fields = ['url', 'email', 'role']
+
+
+class BlogPostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = BlogPost
+        fields = ['url', 'title', 'short_description', 'content', 'owner']
+
+
+class TagSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['url', 'name']
+
