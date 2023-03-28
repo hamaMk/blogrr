a168bccc91df97af95388ce777e783eb3630026d
hamaMk <hamandishemk2014@gmail.com>
2023-03-28 13:46:09 +0200

    feat: Add django admin support to users and api apps

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creation_date', 'last_edited', 'pub_date', 'published', 'owner')
+    list_filter = ('published', )
+
+
+
